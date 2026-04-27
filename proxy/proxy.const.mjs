2efe5087367a30feabcc8327c3ac08c9cc4507d3
Wayne Lam <wayne.lam@bodleian.ox.ac.uk>
2026-04-27 10:23:54 +0100

    merge upstream

--- proxy/proxy.const.mjs
+++ proxy/proxy.const.mjs
@@ -1,7 +1,7 @@
 //set the url of the server you want to test your code with and start the development server using the following command:
 // ng serve --proxy-config ./proxy/proxy.conf.mjs   
 const environments = {
-    'example': 'https://myPrimoVE.com',
+    'oxford_nde': 'https://oxford-psb.primo.exlibrisgroup.com',
   }
   
-  export const PROXY_TARGET = environments['example']; 
+  export const PROXY_TARGET = environments['oxford_nde']; 
